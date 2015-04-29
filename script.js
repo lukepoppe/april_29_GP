8960250129271492bbe086639f78c1feb41ae319
lukepoppe <lukepoppe@hotmail.com>
2015-04-29 15:22:06 -0500

    new

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var apikey = ''; 
-
-
+var interval;
+var maxInt;
 function searchCallback(results) {
 	$('#searchResults').empty();
 	console.log("Here are the results: ", results);
 	for (var i = 0; i < 8; i++) {
 		var platformName = "";
-
+		
 		var platformsObject = results[i].platforms;
 		if(platformsObject == null){
 			platformsObject = [];
@@ -29,21 +29,15 @@ function searchCallback(results) {
 			
 	}
 	$('#searchResults').children().first().removeClass('hidden');
-}
-
-
-
-
-
-
-
-	
+	interval = 0;
+	maxInt = i;
+};	
 
 var userInput = "";
 var apikey = "d40a650b5d8cc7c495d91736f95dee0b8993d809";
 $(document).ready(function() {
 	console.log("Document Ready");
-	$('.btn').on('click', function(){
+	$('.searchBtn').on('click', function(){
 		$('#searchResults').empty();
 		userInput = $('#searchField').val();
 		search(userInput);
@@ -57,13 +51,35 @@ $(document).ready(function() {
 	});
 	$('#searchResults').on('click', '.removeBtn', function(){
 		$(this).parent().fadeOut('slow');
-		});
 	});
+
+	$('.container').on("click", ".right", function (e){
+		e.preventDefault();
+		interval++;
+			if(interval == 8){
+			interval = 0;
+			}
+		$('#searchResults').children().addClass('hidden');
+		$('#searchResults').children('#result' + interval).removeClass('hidden');
+		console.log(interval);
+	});
+	$('.container').on("click", ".left", function (e){
+		e.preventDefault();
+		interval--;
+			if(interval == -1){
+			interval = 7;
+			}
+
+		$('#searchResults').children().addClass('hidden');
+		$('#searchResults').children('#result' + interval).removeClass('hidden');
+		console.log(interval);
+	});
+});
 
 // HELPER FUNCTION
 // Executes a search using 'query' and runs searchCallback on the results of a success.
 function search(query){
-	console.log("Search is firing");
+	
 	$.ajax ({
 	    type: 'GET',
 	    dataType: 'jsonp',
@@ -71,11 +87,11 @@ function search(query){
 	    jsonp: 'json_callback',
 	    url: 'http://www.giantbomb.com/api/search/?format=jsonp&resources=game&api_key=' + apikey +'&query=' + encodeURI(query),
 	    complete: function() {
-	        console.log('ajax complete');
+	    
 	    },
 	    success: function(data) {
 	        searchCallback(data.results);
-	        console.log(data.results);
+	        
 	    }
 	});
 
